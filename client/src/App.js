0813f96a371e0f0ac7249f8364de7450bdf5f603
Dave Quinn <dquinn8@cogeco.ca>
2022-01-14 16:24:28 -0500

    now rendering all components through an Apollo Provider

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,8 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
+     <Router>
       <>
         <Navbar />
         <Switch>
@@ -39,7 +40,8 @@ function App() {
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
-    </Router>
+     </Router>
+    </ApolloProvider>
   );
 }
 
