d8ba26e909bd3e3477b566b41636e68ba9eed4e4
Dave Quinn <dquinn8@cogeco.ca>
2022-01-14 11:41:58 -0500

    got starter files working with production hack

--- server/server.js
+++ server/server.js
@@ -9,13 +9,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// must build from the client directory and set the this ENV variable
+// or the site will produce errors as it only returns the routes it knows or index.html
+// the client/public directory is not the same structure as the build directory
+// this causes calls for manifest.json and javascripts/css etc to all return index.html
+process.env.NODE_ENV = 'production';
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
