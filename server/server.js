4788cbdec08b4aa8e0681efe5c82fb36643f47c2
Dave Quinn <dquinn8@cogeco.ca>
2022-01-17 12:39:53 -0500

    testing new default route take 2

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get("*", function(req, res) {
-    res.render("Error_page");
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 //app.use(routes);
